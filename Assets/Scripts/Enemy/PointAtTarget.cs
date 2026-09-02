a2db4f5fc09b5c0efaf01aa24426b5a738b6375a
Photon616 <yisollove@gmail.com>
2026-09-02 21:52:52 +0900

    laser pointer and prefab-fy

--- Assets/Scripts/Enemy/PointAtTarget.cs
+++ Assets/Scripts/Enemy/PointAtTarget.cs
@@ -3,22 +3,18 @@ using UnityEngine;
 
 public class PointAtTarget : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    [SerializeField] private LineRenderer lr;
 
     public void PointAt(Vector3 target)
     {
-        Vector3 dirVector = transform.position - target;
+        Vector3 dirVector = target - transform.position;
         float angle = Mathf.Atan2(dirVector.y, dirVector.x);
         transform.rotation = quaternion.Euler(0, 0, angle);
+
+        LayerMask mask = LayerMask.GetMask("Default");
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, dirVector.normalized, 100f, mask);
+        lr.SetPosition(0, transform.position);
+        lr.SetPosition(1, hit.point);
+        Debug.Log(hit.collider);
     }
 }
