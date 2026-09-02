ee333d202527751eea1b9ffddef9b4e45e5f9682
Photon616 <113816102+Photon616@users.noreply.github.com>
2026-09-02 13:25:19 +0900

    bullet hit

--- Assets/Scripts/Enemy/EnemyBullet.cs
+++ Assets/Scripts/Enemy/EnemyBullet.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class EnemyBullet : MonoBehaviour
 {
     [SerializeField] private float speed = 1f;
+    [SerializeField] private float aliveTimer = 16f;
     private Vector2 direction;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -15,6 +16,31 @@ public class EnemyBullet : MonoBehaviour
     void Update()
     {
         transform.position += new Vector3(direction.x, direction.y) * speed * Time.deltaTime;
+        aliveTimer -= Time.deltaTime;
+        if (aliveTimer <= 0)
+        {
+            DestroySelf();
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        // Debug.Log("collided");
+        if (collision.gameObject.CompareTag("Floor") || collision.gameObject.CompareTag("Wall"))
+        {
+            // Debug.Log("collided with wall");
+            DestroySelf();
+        }
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            // Debug.Log("collided with player");
+            DestroySelf();
+        }
+    }
+
+    private void DestroySelf()
+    {
+        Destroy(gameObject);
     }
 
     public void SetDirection(Vector2 dir)
